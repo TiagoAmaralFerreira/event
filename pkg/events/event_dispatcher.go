7974d1626b4fc92a2817c1a2f688216af55abeea
Tiago <tiago.amaral2901@gmail.com>
2025-02-24 13:30:05 -0300

    feat: test from dispatcher

--- pkg/events/event_dispatcher.go
+++ pkg/events/event_dispatcher.go
@@ -27,6 +27,27 @@ func (ed *EventDispatcher) Register(eventName string, handle EventHandlerInterfa
 	return nil
 }
 
+func (ed *EventDispatcher) Has(eventName string, handler EventHandlerInterface) bool {
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (ed *EventDispatcher) Dispatch(event EventInterface) error {
+	if handlers, ok := ed.handlers[event.GetName()]; ok {
+		for _, h := range handlers {
+			h.Handle(event)
+		}
+		return nil
+	}
+	return errors.New("event not registered")
+}
+
 func (ed *EventDispatcher) Clear() {
 	ed.handlers = make(map[string][]EventHandlerInterface)
 }
